ac78449fdbd5aeb66137b60fee07b7f979f38fdd
Souleyman BEN HADJ BELGACEM <souleyman@MacBook-Pro-de-Souleyman.local>
2023-06-02 18:02:01 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import red from "./images/red.svg";
+import blue from "./images/blue.svg";
+import cyan from "./images/cyan.svg";
+import green from "./images/green.svg";
+import orange from "./images/orange.svg";
+import pink from "./images/pink.svg";
+import white from "./images/white.svg";
+import yellow from "./images/yellow.svg";
+
+import "./App.css";
 
 function App() {
+  const amogus = [red, blue, cyan, green, orange, pink, white, yellow];
+  const [currentAmogus, setCurrentAmogus] = useState(red);
+
+  const changeAmogus = () => {
+    setCurrentAmogus(amogus[Math.floor(Math.random() * amogus.length)]);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={changeAmogus}>
+          <img src={currentAmogus} className="App-logo" alt="logo" />
+        </button>
       </header>
     </div>
   );
